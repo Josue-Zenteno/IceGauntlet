4becac9bcc6b628f328a0695a1fa442fd0acf54c
spicy <josuotp@gmail.com>
2020-11-28 12:50:05 +0100

    Version final de AuthClient.py
    Co-authored-by: Sergio-Silvestre <72158387+Sergio-Silvestre@users.noreply.github.com>
    Co-authored-by: marina-prieto <marinaprieto999@gmail.com>

--- src/AuthClient.py
+++ src/AuthClient.py
@@ -16,47 +16,70 @@ import IceGauntlet
 
 class Client(Ice.Application):
     def run(self, argv):
-        parser = argparse.ArgumentParser()
-        parser.add_argument("Proxy", help="Aqui va el proxy generado por el server")
-        args = parser.parse_args()
+        try:
+            args = self.parseArgs(argv)
 
-        proxy = self.communicator().stringToProxy(args.Proxy)
-        print(args.Proxy)
-        authServer = IceGauntlet.AuthenticationPrx.checkedCast(proxy)
-        
-        if not authServer:
-            raise RuntimeError('Invalid proxy')
-        
-        self.printMenu(authServer)
-
-        return 0
-
-        
-    def printMenu(self, authServer):
-        option = input("Select an option: \n1.Obtener token de autorización\n2.Cambiar contraseña\n")
-        if option == '1':
-            user = input("Introduce tu nombre: ")
-            password = getpass.getpass(prompt="Introduce contraseña:")
-            passwordHash = hashlib.sha256()
-            passwordHash.update(password.encode())
-            token = authServer.getNewToken(user, passwordHash.digest().decode())
-            print(token)
+            proxy = self.communicator().stringToProxy(args.Proxy)
+            authServer = IceGauntlet.AuthenticationPrx.checkedCast(proxy)
+            print("\nTe has conectado al Proxy: " + args.Proxy + "\n(Si es la primera vez que accedes, debes obtener primero una contraseña)")
             
+            if not authServer:
+                raise RuntimeError('Invalid proxy')
+            
+            self.printMenu(authServer)
+            return 0
 
-        if option == '2':
-            user = input("Introduce tu nombre: ")
-            currentPass = getpass.getpass(prompt="Introduce contraseña:")
-            if currentPass == "":
-                password = None
-            else:
-                currentPassHash = hashlib.sha256()
-                currentPassHash.update(currentPass.encode())
-                password = currentPassHash.digest().decode()
-                
-            newPass = getpass.getpass(prompt="Introduce contraseña nueva:")
-            newPassHash = hashlib.sha256()
-            newPassHash.update(newPass.encode())
-            authServer.changePassword(user, password, newPassHash.digest().decode())
+        except IceGauntlet.Unauthorized:
+            print("Usuario y/o Contraseña no válida")
+            return 1
+        except Ice.Exception:
+            print("Proxy no disponible en este momento\nException: Connection Refused")
+            return 2
+        except RuntimeError:
+            return 3
+        except EOFError:
+            return 4
+  
+    def printMenu(self, authServer):
+        option = input("\n¿Qué quieres hacer?:\n1.Obtener token de autorización\n2.Cambiar contraseña\n")
+        if option == '1':
+            user = input("Introduce tu Usuario: ")
+            currentPass = getpass.getpass(prompt="Introduce tu Contraseña:")
+            passwordHash = self.hashPassStr(currentPass)
+            
+            token = authServer.getNewToken(user, passwordHash)
+            self.saveToken(token)
 
+        elif option == '2':
+            user = input("Introduce tu Usuario: ")
+            currentPass = getpass.getpass(prompt="Introduce tu Contraseña actual:")
+            currentPassHash = self.hashPassStr(currentPass)
+            newPass = getpass.getpass(prompt="Introduce Contraseña nueva:")
+            newPassHash = self.hashPassStr(newPass)
+
+            authServer.changePassword(user, currentPassHash, newPassHash)
+        else:
+            print("Ninguna opción seleccionada")
+
+    @staticmethod
+    def hashPassStr(password):
+        passwordHasher = hashlib.sha256()
+        passwordHasher.update(password.encode())
+        passwordHash = passwordHasher.digest()
+        return str(passwordHash)
+
+    @staticmethod
+    def parseArgs(argv):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("Proxy", help="Aqui va el proxy generado por el Athentication Server")
+        args = parser.parse_args()
+        return args
+    
+    @staticmethod
+    def saveToken(token):
+        tokenTxt = open("token.txt", "w")
+        tokenTxt.write(token)
+        tokenTxt.close()
+        print("Tu token de identificación es: "+token+"\nTambién se ha guardado en el archivo token.txt")
 
 sys.exit(Client().main(sys.argv))
