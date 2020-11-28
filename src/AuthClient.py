d0b553afd664f6524d14eeddc712043912f48117
Sergio-Silvestre <Sergio.Silvestre@alu.uclm.es>
2020-11-28 10:45:33 +0100

    Segundo intento AuthClient.py
    Co-authored-by: Josue Carlos Zenteno Yave <61275784+Josue-Zenteno@users.noreply.github.com>
    Co-authored-by: marina-prieto <marinaprieto999@gmail.com>

--- src/AuthClient.py
+++ src/AuthClient.py
@@ -7,6 +7,8 @@
 import sys
 import Ice
 import argparse
+import getpass
+import hashlib
 Ice.loadSlice('IceGauntlet.ice')
 # pylint: disable=E0401
 # pylint: disable=C0413
@@ -14,15 +16,47 @@ import IceGauntlet
 
 class Client(Ice.Application):
     def run(self, argv):
-        proxy = self.communicator().stringToProxy(argv[1])
-        print(argv[1])
+        parser = argparse.ArgumentParser()
+        parser.add_argument("Proxy", help="Aqui va el proxy generado por el server")
+        args = parser.parse_args()
+
+        proxy = self.communicator().stringToProxy(args.Proxy)
+        print(args.Proxy)
         authServer = IceGauntlet.AuthenticationPrx.checkedCast(proxy)
         
         if not authServer:
             raise RuntimeError('Invalid proxy')
         
-        self.printMenu()
+        self.printMenu(authServer)
+
+        return 0
 
         
-    def printMenu(self):
-        pass
+    def printMenu(self, authServer):
+        option = input("Select an option: \n1.Obtener token de autorización\n2.Cambiar contraseña\n")
+        if option == '1':
+            user = input("Introduce tu nombre: ")
+            password = getpass.getpass(prompt="Introduce contraseña:")
+            passwordHash = hashlib.sha256()
+            passwordHash.update(password.encode())
+            token = authServer.getNewToken(user, passwordHash.digest().decode())
+            print(token)
+            
+
+        if option == '2':
+            user = input("Introduce tu nombre: ")
+            currentPass = getpass.getpass(prompt="Introduce contraseña:")
+            if currentPass == "":
+                password = None
+            else:
+                currentPassHash = hashlib.sha256()
+                currentPassHash.update(currentPass.encode())
+                password = currentPassHash.digest().decode()
+                
+            newPass = getpass.getpass(prompt="Introduce contraseña nueva:")
+            newPassHash = hashlib.sha256()
+            newPassHash.update(newPass.encode())
+            authServer.changePassword(user, password, newPassHash.digest().decode())
+
+
+sys.exit(Client().main(sys.argv))
